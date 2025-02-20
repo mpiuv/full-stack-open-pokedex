a64f0e5c7b411089f16b3da1450c4b11933a5248
T. L. <43107946+mpiuv@users.noreply.github.com>
2025-02-20 13:40:08 +0200

    11.12

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
